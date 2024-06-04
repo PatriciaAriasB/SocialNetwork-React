199cdbeff6a43df9a7e622d1cbf6051f19d24018
Duchicus <mpiernash@gmail.com>
2024-06-04 15:33:10 +0200

    avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import React from "react";
+import "./App.css";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
