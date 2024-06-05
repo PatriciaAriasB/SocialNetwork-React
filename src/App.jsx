a98fc502f44815f682d89f29d3cdfb2a34124c0d
Duchicus <mpiernash@gmail.com>
2024-06-05 11:43:49 +0200

    things

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Posts from "./components/Posts/Posts";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/posts" element={<Posts />} />
         </Routes>
       </BrowserRouter>
     </div>
